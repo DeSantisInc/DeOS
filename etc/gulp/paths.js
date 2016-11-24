45198813266871163cfc23ffa60cf58bdecae162
Pablo de Santis <andrew@desantis.email>
2016-11-24 13:11:15 -0600

    refactor: js

---
+++ etc/gulp/paths.js
@@ -0,0 +1,39 @@
+export const paths = {
+  "dirs": {
+    "dist": "app",
+    "lib": "build/es5"
+  },
+  "files": {
+    "client": {
+      "all": "src/client/**/*.js?(x)",
+      "bundle": "app/bundle.js?(.map)",
+      "entry": "src/client/app.jsx"
+    },
+    "config": {
+      "gulp": "Gulpfile.babel.js",
+      "webpack": "webpack.config.babel.js"
+    },
+    "server": {
+      "all": "src/server/**/*.js?(x)"
+    },
+    "shared": {
+      "all": "src/shared/**/*.js?(x)"
+    },
+    "tests": {
+      "all": "build/es5/test/**/*.js"
+    }
+  }
+};
+
+export const allConfigJS = [
+  paths.files.config.gulp,
+  paths.files.config.webpack
+];
+
+export const allSrcJS = 'src/**/*.js?(x)';
+
+export const allJS = [allSrcJS].concat(allConfigJS);
+
+export const buildDeps = ['lint', 'clean'];
+
+export const toClean = [paths.files.client.bundle, paths.dirs.lib];
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,52 +6,30 @@ import eslint from 'gulp-eslint';
 import flow from 'gulp-flowtype';
 import mocha from 'gulp-mocha';
 import del from 'del';
-
 import webpack from 'webpack-stream';
+
 import webpackConfig from './webpack.config.babel';
+import { paths, allJS, allSrcJS, buildDeps, toClean } from './etc/gulp/paths';
 
-const paths = {
-  allSrcJs: 'src/**/*.js?(x)',
-  allLibTests: 'build/es5/test/**/*.js',
-  serverSrcJs: 'src/server/**/*.js?(x)',
-  sharedSrcJs: 'src/shared/**/*.js?(x)',
-  clientBundle: 'dist/client-bundle.js?(.map)',
-  clientEntryPoint: 'src/client/app.jsx',
-  gulpFile: 'gulpfile.babel.js',
-  webpackFile: 'webpack.config.babel.js',
-  libDir: 'build/es5',
-  distDir: 'dist',
-};
+gulp.task('lint', () => gulp.src(allJS)
+                            .pipe(eslint())
+                            .pipe(eslint.format())
+                            .pipe(eslint.failAfterError())
+                            .pipe(flow({ abort: true })));
 
-gulp.task('lint', () =>
-  gulp.src([paths.allSrcJs, paths.gulpFile, paths.webpackFile])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-    .pipe(flow({ abort: true }))
-);
+gulp.task('clean', () => del(toClean));
 
-gulp.task('clean', () => del([paths.libDir, paths.clientBundle]));
+gulp.task('build', buildDeps, () => gulp.src(allSrcJS)
+                                        .pipe(babel())
+                                        .pipe(gulp.dest(paths.dirs.lib)));
 
-gulp.task('build', ['lint', 'clean'], () =>
-  gulp.src(paths.allSrcJs)
-    .pipe(babel())
-    .pipe(gulp.dest(paths.libDir))
-);
+gulp.task('test', ['build'], () => gulp.src(paths.files.tests.all)
+                                       .pipe(mocha()));
 
-gulp.task('test', ['build'], () =>
-  gulp.src(paths.allLibTests)
-    .pipe(mocha())
-);
+gulp.task('main', ['test'], () => gulp.src(paths.files.client.entry)
+                                      .pipe(webpack(webpackConfig))
+                                      .pipe(gulp.dest(paths.dirs.dist)));
 
-gulp.task('main', ['test'], () =>
-  gulp.src(paths.clientEntryPoint)
-    .pipe(webpack(webpackConfig))
-    .pipe(gulp.dest(paths.distDir))
-);
-
-gulp.task('watch', () => {
-  gulp.watch(paths.allSrcJs, ['main']);
-});
+gulp.task('watch', () => gulp.watch(allSrcJS, ['main']));
 
 gulp.task('default', ['watch', 'main']);

c3617005fa9508e619254bb67d708316d79efb70
Pablo de Santis <andrew@desantis.email>
2016-11-26 16:35:13 -0600

    clean: layout

--- src/deos/check.mk
+++ src/deos/check.mk
@@ -1,8 +1,8 @@
-__FILES:=$(addprefix check.,$(FILES))
+CHECK_FILES:=$(addprefix check.,$(FILES))
 
-deos.check: $(__FILES)
+deos.check: $(CHECK_FILES)
 
-$(__FILES):
+$(CHECK_FILES):
 	@[ -f $(BASEDIR)/$(subst check.,,$@) ]\
 	&& echo "$(subst check.,[PASS] CHECK -> ,$@)"\
 	|| echo "$(subst check.,[FAIL] CHECK -> ,$@)";

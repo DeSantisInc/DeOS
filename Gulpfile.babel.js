ffd4559aeed0867ba43f5ad39360c46469d1ef8d
Andrew T. DeSantis <andrew@desantis.email>
2016-11-28 17:49:39 -0600

    add: gulp build

--- Gulpfile.babel.js
+++ Gulpfile.babel.js
@@ -5,6 +5,7 @@ import babel from 'gulp-babel';
 import pug from 'gulp-pug';
 import rename from 'gulp-rename';
 import del from 'del';
+
 import { paths, toClean } from './src/gulp/config.paths';
 
 gulp.task('pug', () => gulp.src(paths.files.client.pug.in)
@@ -14,9 +15,20 @@ gulp.task('pug', () => gulp.src(paths.files.client.pug.in)
 
 gulp.task('clean', () => del(toClean));
 
-gulp.task('build', ['pug'], () => gulp.src(allSrcJS)
-                                      .pipe(babel())
-                                      .pipe(gulp.dest(paths.dirs.lib)));
+gulp.task('build', ['pug'], () => {
+  gulp.src(paths.files.client.js.all)
+      .pipe(babel())
+      .pipe(gulp.dest(paths.dirs.es5.client));
+  gulp.src(paths.files.common.js.all)
+      .pipe(babel())
+      .pipe(gulp.dest(paths.dirs.es5.common));
+  gulp.src(paths.files.render.js.all)
+      .pipe(babel())
+      .pipe(gulp.dest(paths.dirs.es5.render));
+  gulp.src(paths.files.server.js.all)
+      .pipe(babel())
+      .pipe(gulp.dest(paths.dirs.es5.server));
+});
 
 gulp.task('main', ['clean', 'build']);
 
--- src/gulp/config.paths.js
+++ src/gulp/config.paths.js
@@ -4,34 +4,43 @@ export const paths = {
     'es5': {
       'client': 'var/build/es5/client',
       'common': 'var/build/es5/common',
-      'renderer': 'var/build/es5/renderer',
+      'render': 'var/build/es5/render',
       'server': 'var/build/es5/server'
     },
     'lib': 'var/build/es5'
   },
   'files': {
     'client': {
+      'js': {
+        'all': 'src/client/**/*.js?(x)'
+      },
       'pug': {
         'in': 'src/client/index.pug',
         'out': 'index.min.html'
-      },
-      'server': {
-        'js': {
-          'all': 'src/server/**/*.js?(x)'
-        }
+      }
+    },
+    'common': {
+      'js': {
+        'all': 'src/common/**/*.js?(x)'
+      }
+    },
+    'render': {
+      'js': {
+        'all': 'src/render/**/*.js?(x)'
+      }
+    },
+    'server': {
+      'js': {
+        'all': 'src/server/**/*.js?(x)'
       }
     }
   }
 }
 
-export const allSrcJS = [
-  paths.files.server.js.all
-]
-
 export const toClean = [
   `app/${paths.files.client.pug.out}`,
   paths.dirs.es5.client,
   paths.dirs.es5.common,
-  paths.dirs.es5.renderer,
+  paths.dirs.es5.render,
   paths.dirs.es5.server
 ]

3f37aac42a05c70452a5548092f141f4fadd1855
Andrew T. DeSantis <andrew@desantis.email>
2016-11-28 17:30:31 -0600

    prep: gulp build

--- src/gulp/config.paths.js
+++ src/gulp/config.paths.js
@@ -1,6 +1,13 @@
 export const paths = {
   'dirs': {
-    'dist': 'app'
+    'dist': 'app',
+    'es5': {
+      'client': 'var/build/es5/client',
+      'common': 'var/build/es5/common',
+      'renderer': 'var/build/es5/renderer',
+      'server': 'var/build/es5/server'
+    },
+    'lib': 'var/build/es5'
   },
   'files': {
     'client': {
@@ -13,5 +20,9 @@ export const paths = {
 }
 
 export const toClean = [
-  `app/${paths.files.client.pug.out}`
+  `app/${paths.files.client.pug.out}`,
+  paths.dirs.es5.client,
+  paths.dirs.es5.common,
+  paths.dirs.es5.renderer,
+  paths.dirs.es5.server
 ]

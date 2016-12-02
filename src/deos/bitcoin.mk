6a7fcec62f74d0fed0ca8224e217e375371660ec
Andrew T. DeSantis <andrew@desantis.email>
2016-12-01 22:10:26 -0600

    refactor: bitcoin

--- src/deos/bitcoin.mk
+++ src/deos/bitcoin.mk
@@ -9,13 +9,15 @@ bitcoin.install: bitcoin.clone
 	@$(PRINT) purple $@ start
 	mv $(BASEDIR)/.tmp/bitcoin/doc $(BASEDIR)/var/docs/bitcoin
 	mv $(BASEDIR)/.tmp/bitcoin/README.md $(BASEDIR)/var/docs/bitcoin/README.md
-	mv $(BASEDIR)/.tmp/bitcoin/lib/crypto $(BASEDIR)/src/crypto/lib
+	mv $(BASEDIR)/.tmp/bitcoin/lib/crypto/*.c $(BASEDIR)/src/crypto/
+	mv $(BASEDIR)/.tmp/bitcoin/include/ccoin/crypto/*.h $(BASEDIR)/src/crypto/
+	rm -rf $(BASEDIR)/.tmp/bitcoin/include/ccoin/crypto/
+	rm -rf $(BASEDIR)/.tmp/bitcoin/lib/crypto/
 	mv $(BASEDIR)/.tmp/bitcoin/lib/net $(BASEDIR)/src/net/lib
 	mv $(BASEDIR)/.tmp/bitcoin/lib $(BASEDIR)/src/bitcoin/lib
 	mv $(BASEDIR)/.tmp/bitcoin/src/*.c $(BASEDIR)/src/bitcoin/
 	mv $(BASEDIR)/.tmp/bitcoin/src/*.h $(BASEDIR)/src/bitcoin/
 	mv $(BASEDIR)/.tmp/bitcoin/include/ccoin/net $(BASEDIR)/src/net/include
-	mv $(BASEDIR)/.tmp/bitcoin/include/ccoin/crypto $(BASEDIR)/src/crypto/include
 	mv $(BASEDIR)/.tmp/bitcoin/include/ccoin $(BASEDIR)/src/bitcoin/include
 	mv $(BASEDIR)/.tmp/bitcoin/test $(BASEDIR)/src/test/bitcoin
 	@$(PRINT) purple $@ stop

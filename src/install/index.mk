8c394fe12556200587f44cd90e08ff8d33f55d12
Andrew T. DeSantis <andrew@desantis.email>
2016-12-11 16:21:08 -0600

    rm: .virtualenvs/

--- src/install/index.mk
+++ src/install/index.mk
@@ -1,6 +1,6 @@
-__PATH__=src/install
+_PATH=src/install
 
-include $(__PATH__)/bitcoin.mk
-include $(__PATH__)/test.mk
-include $(__PATH__)/vbox.mk
-include $(__PATH__)/zerotier.mk
+include $(_PATH)/bitcoin.mk
+include $(_PATH)/test.mk
+include $(_PATH)/vbox.mk
+include $(_PATH)/zerotier.mk

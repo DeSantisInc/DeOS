dd48848b05d9c703d2dee371e41663ddf29affbc
Andrew T. DeSantis <andrew@desantis.email>
2016-12-21 08:50:24 -0600

    mv: test/src/web -> test/web

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,8 +36,7 @@ deos_init() {
   for path in .deos .deos/bin .deos/obj .deos/venv\
               .deos/bin/darwin .deos/bin/vagrant .deos/bin/travis\
               .deos/obj/darwin .deos/obj/vagrant .deos/obj/travis\
-              .deos/venv/darwin .deos/venv/vagrant .deos/venv/travis\
-              test/src
+              .deos/venv/darwin .deos/venv/vagrant .deos/venv/travis
   do
     [ ! -d "$path" ] && mkdir $path
   done
@@ -47,7 +46,7 @@ deos_clean() {
   [ -d ".deos" ] && rm -rf .deos/
   [ -d "src/web" ] && rm -rf src/web/
   [ -d "doc/web" ] && rm -rf doc/web/
-  [ -d "test/src/web" ] && rm -rf test/src/web/
+  [ -d "test/web" ] && rm -rf test/web/
   [ -f "boot/init.lz" ] && rm boot/init.lz
   [ -f "boot/python.lz" ] && rm boot/python.lz
   [ -f "src/example.sh" ] && rm src/example.sh

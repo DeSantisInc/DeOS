4f625c0f12aa9d020eef3096cdee6db15627cea6
Andrew T. DeSantis <andrew@desantis.email>
2016-12-21 01:49:49 -0600

    update: src/deos.py

--- src/deos.py
+++ src/deos.py
@@ -22,12 +22,13 @@ def build(template,data):
                          ).replace('\n\nendif','\nendif'
                          )[1:]
 
-def get_name(data,debug=False):
+def get_environment(data,raw,debug=False):
     try:
         res=None
-        name=data.split('\n')[0]
-        if name[0:3]=='# `' and name[-1]=='`':
-            res=name[3:-1]
+        env=data.split('## Environment\n\n```yaml\n')[1].split(\
+            '```\n\n## Template')[0]
+        if isinstance(env, basestring):
+            res=yaml.safe_load(env)
     except:
         res=None
     else:
@@ -36,13 +37,12 @@ def get_name(data,debug=False):
     finally:
         return res
 
-def get_environment(data,raw,debug=False):
+def get_name(data,debug=False):
     try:
         res=None
-        env=data.split('## Environment\n\n```yaml\n')[1].split(\
-            '```\n\n## Template')[0]
-        if isinstance(env, basestring):
-            res=yaml.safe_load(env)
+        name=data.split('\n')[0]
+        if name[0:3]=='# `' and name[-1]=='`':
+            res=name[3:-1]
     except:
         res=None
     else:
@@ -82,37 +82,33 @@ def get_template(data,raw,debug=False):
     finally:
         return res
 
-def load(fname):
-    debug=False
-    data,env,name,raw,schema,template=None,None,None,None,None,None
-    with open(fname) as f:
-        raw=f.read()
-    if isinstance(raw,basestring):
-        data=raw
-        name=get_name(data)
-        if isinstance(name,basestring):
-            raw=raw.split('\n')[1:]
-            if '## Environment' in raw and '## Schema' in raw and\
-                '## Template' in raw:
-                env=get_environment(data,raw,debug=False)
-                schema=get_schema(data,raw,debug=False)
-                if isinstance(env, dict) and isinstance(schema, dict):
-                    jsonschema.validate(env, schema)
-                    if debug:
-                        print(json.dumps(env, sort_keys=True, indent=2))
-                    template=get_template(data,raw,debug=debug)
-                    if isinstance(template,basestring):
-                        code = build(template,env)
-                        with open('var/build/%s'%name,'w') as f:
-                            f.write(code)
-    return data
-
 def main():
     config=configobj.ConfigObj('Deosfile')
     for key,value in config.iteritems():
         if key not in ('author','version') and 'type' in value:
+            debug=False
+            data,env,name,raw,schema,template=None,None,None,None,None,None
             if value['type']=='make':
-                load(value['meta'])
+                with open(value['meta']) as f:
+                    raw=f.read()
+            if isinstance(raw,basestring):
+                data=raw
+                name=get_name(data)
+                if isinstance(name,basestring):
+                    raw=raw.split('\n')[1:]
+                    if '## Environment' in raw and '## Schema' in raw and\
+                        '## Template' in raw:
+                        env=get_environment(data,raw,debug=False)
+                        schema=get_schema(data,raw,debug=False)
+                        if isinstance(env,dict) and isinstance(schema,dict):
+                            jsonschema.validate(env, schema)
+                            if debug:
+                                print(json.dumps(env,sort_keys=True,indent=2))
+                            template=get_template(data,raw,debug=debug)
+                            if isinstance(template,basestring):
+                                code = build(template,env)
+                                with open('var/build/%s'%name,'w') as f:
+                                    f.write(code)
 
 if __name__ == "__main__":
     main()

77b8b8a4da0f7617f873993a881b8a3d21b5bb6b
Andrew T. DeSantis <andrew@desantis.email>
2016-12-21 01:45:41 -0600

    update: deos.py

--- src/deos.py
+++ src/deos.py
@@ -4,78 +4,115 @@
 from __future__ import print_function
 
 import configobj
-import simplejson as json
 import jsonschema
 import web
+
+import simplejson as json
 import ruamel.yaml as yaml
 
-def load_config(fname):
-    config = configobj.ConfigObj(fname)
-    return config
-
-def read(fname):
-    data = open(TEMPLATE_PATH+fname).read()
-    data = data.replace('$','$$'
-              ).replace('Δ with', '$def with'
-              ).replace('Δ','$')
-    return data
-
-def write(fname, code):
-    with open(fname, 'w') as f:
-        f.write(code)
-
-def render(fname):
-    raw = read(fname)
-    code = web.template.Template(raw)
-    return code
-
-def build(fname, data):
-    code = render(fname)
-    return str(code(data)).replace(8*' ','\t'
-                         ).replace('$(False)', '$(FALSE)'
-                         ).replace('$(True)', '$(TRUE)'
-                         ).replace('\n\nifeq', '\nifeq'
-                         ).replace('\n\nelse', '\nelse'
-                         ).replace('\n\nendif', '\nendif'
+def build(template,data):
+    code=web.template.Template(template.replace('$','$$'
+                                      ).replace('Δ with', '$def with'
+                                      ).replace('Δ','$'))
+    return str(code(data)).replace(4*' ','\t'
+                         ).replace('$(False)','$(FALSE)'
+                         ).replace('$(True)','$(TRUE)'
+                         ).replace('\n\nifeq','\nifeq'
+                         ).replace('\n\nelse','\nelse'
+                         ).replace('\n\nendif','\nendif'
                          )[1:]
 
-def get_name(data, debug=False):
-    res = None
+def get_name(data,debug=False):
     try:
-        name = data.split('\n')[0]
-        if name[0:3] == '# `' and name[-1] == '`':
-            res = name[3:-1]
+        res=None
+        name=data.split('\n')[0]
+        if name[0:3]=='# `' and name[-1]=='`':
+            res=name[3:-1]
     except:
-        name = None
+        res=None
     else:
         if debug:
-            print(name)
+            print(res)
+    finally:
+        return res
+
+def get_environment(data,raw,debug=False):
+    try:
+        res=None
+        env=data.split('## Environment\n\n```yaml\n')[1].split(\
+            '```\n\n## Template')[0]
+        if isinstance(env, basestring):
+            res=yaml.safe_load(env)
+    except:
+        res=None
+    else:
+        if debug:
+            print(res)
+    finally:
+        return res
+
+def get_schema(data,raw,debug=False):
+    try:
+        res=None
+        schema=data.split('## Schema\n\n```yaml\n')[1].split(\
+            '```\n\n## Environment')[0]
+        if isinstance(schema,basestring):
+            res=yaml.safe_load(schema)
+    except:
+        res=None
+    else:
+        if debug:
+            print(res)
+    finally:
+        return res
+
+def get_template(data,raw,debug=False):
+    try:
+        res=''
+        template=data.split('## Template\n\n```')[1]
+        line=template.split('\n')
+        for i in range(0,len(line)):
+            if i!=0 and i!=len(line)-2:
+                res=res+line[i]+'\n'
+    except:
+        res=None
+    else:
+        if debug:
+            print(res)
     finally:
         return res
 
 def load(fname):
-    data, name, raw, schema = None, None, None, None
+    debug=False
+    data,env,name,raw,schema,template=None,None,None,None,None,None
     with open(fname) as f:
-        raw = f.read()
-    if isinstance(raw, basestring):
-        data = raw
-        name = get_name(data)
-        if isinstance(name, basestring):
-            raw = raw.split('\n')[1:]
-            if '## Environment' in raw:
-                print('env: found')
-            if '## Schema' in raw:
-                print('schema: found')
-            if '## Template' in raw:
-                print('template: found')
+        raw=f.read()
+    if isinstance(raw,basestring):
+        data=raw
+        name=get_name(data)
+        if isinstance(name,basestring):
+            raw=raw.split('\n')[1:]
+            if '## Environment' in raw and '## Schema' in raw and\
+                '## Template' in raw:
+                env=get_environment(data,raw,debug=False)
+                schema=get_schema(data,raw,debug=False)
+                if isinstance(env, dict) and isinstance(schema, dict):
+                    jsonschema.validate(env, schema)
+                    if debug:
+                        print(json.dumps(env, sort_keys=True, indent=2))
+                    template=get_template(data,raw,debug=debug)
+                    if isinstance(template,basestring):
+                        code = build(template,env)
+                        with open('var/build/%s'%name,'w') as f:
+                            f.write(code)
     return data
 
 def main():
-    config = load_config('Deosfile')
-    for key, value in config.iteritems():
-        if key not in ('author', 'version') and 'type' in value:
-            if value['type'] == 'make':
-                data = load(value['meta'])
+    config=configobj.ConfigObj('Deosfile')
+    for key,value in config.iteritems():
+        if key not in ('author','version') and 'type' in value:
+            if value['type']=='make':
+                load(value['meta'])
 
 if __name__ == "__main__":
     main()

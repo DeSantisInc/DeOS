40ab4c8147709807aaf6a27c8980d7d5857708df
Andrew T. DeSantis <andrew@desantis.email>
2016-12-20 23:46:55 -0600

    add: bin

--- Makefile
+++ Makefile
@@ -2,10 +2,21 @@ export MAKEFLAGS=--no-print-directory
 
 include .deosrc
 
-ifeq ($(DeOS_HOST_OS),Darwin)
-	include src/make/darwin.mk
-endif
+all: clean install build venv lint
+	@(python src/deos.py)
 
-ifeq ($(DeOS_HOST_OS),Linux)
-	include src/make/travis.mk
-endif
+clean:
+	@([ -d ".deos" ] && $(DeOS_RM_DOTDEOS) || echo "$@:else")
+
+install:
+	@([ ! -x "$(DeOS_BIN_TRAVIS)" ] && $(DeOS_ADD_TRAVIS) || echo "$@:else")
+
+build:
+	@([ ! -d ".deos" ] && $(DeOS_ADD_DOTDEOS) || echo "$@:else")
+
+venv:
+	@([ -d ".deos/venv" ] && rm -rf .deos/venv || echo "$@:else")
+	@([ ! -d ".deos/venv" ] && mkdir .deos/venv .deos/venv/darwin .deos/venv/vagrant .deos/venv/travis || echo "$@:else")
+
+lint:
+	@(travis lint .travis.yml)
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,77 @@
 #!/bin/sh
 
-echo "hello, world!"
+HOST_OS=$(uname)
+
+EXIT_SUCCESS() {
+  exit 0
+}
+
+EXIT_FAILURE() {
+  exit 1
+}
+
+deos_bin() {
+  for path in .deos/bin/darwin .deos/bin/vagrant .deos/bin/travis
+  do
+    [ ! -f "$path/deos" ] && cp src/deos.py $path/deos
+    [ ! -f "$path/logger" ] && cp src/logger.py $path/logger
+    [ ! -f "$path/print" ] && cp src/print.py $path/print
+    [ ! -f "$path/spinner" ] && cp src/spinner.sh $path/spinner
+    chmod +x $path/deos # add: if-darwin
+    chmod +x $path/logger # add: if-darwin
+    chmod +x $path/print # add: if-darwin
+    chmod +x $path/spinner # add: if-darwin
+  done
+}
+
+deos_venv() {
+  path=.deos/venv/$1
+  bin=virtualenv
+  venv=default
+  flag=--no-site-packages
+  [ ! -d "$path/$venv" ] && (cd $path && $bin $venv $flag)
+}
+
+deos_init() {
+  for path in .deos .deos/bin .deos/obj .deos/venv\
+              .deos/bin/darwin .deos/bin/vagrant .deos/bin/travis\
+              .deos/obj/darwin .deos/obj/vagrant .deos/obj/travis\
+              .deos/venv/darwin .deos/venv/vagrant .deos/venv/travis
+  do
+    [ ! -d "$path" ] && mkdir $path
+  done
+}
+
+deos_clean() {
+  path=.deos
+  [ -d "$path" ] && rm -rf $path
+}
+
+deos_darwin() {
+  deos_clean
+  deos_init
+  deos_bin
+  #deos_venv "darwin"
+  .deos/bin/darwin/deos
+  EXIT_SUCCESS
+}
+
+deos() {
+  case "${HOST_OS:-nil}" in
+    Darwin)
+      deos_darwin
+    ;;
+    Linux)
+      EXIT_FAILURE
+    ;;
+    *)
+      EXIT_FAILURE
+    ;;
+  esac
+}
+
+main() {
+  deos
+}
+
+main

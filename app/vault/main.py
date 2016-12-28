1d087066b26314309f9d3c32cb96fe27dd94453f
Andrew DeSantis <atd@gmx.it>
2016-12-28 05:14:27 -0600

    add-vault: templates

--- app/vault/main.py
+++ app/vault/main.py
@@ -2,11 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-
 import web
+import simplejson as json
+import ruamel.yaml as yaml
+
+TEMPLATES=['templates/add_password_dialog.ui']
 
 def main():
-    print(web)
+    for template in TEMPLATES:
+        with open(template) as f:
+            raw=f.read()
+            code=web.template.Template(raw)
+            data=None
+            print(code(data))
 
 if __name__=="__main__":
     main()

8485b42ffe94a69daa08677e79a76e27efaf87ad
Andrew DeSantis <atd@gmx.it>
2016-12-28 14:44:07 -0600

    add-vault: partials

--- app/vault/main.py
+++ app/vault/main.py
@@ -12,40 +12,49 @@ import ruamel.yaml as yaml
 
 TEMPLATES=['add_password_dialog']
 
-def render_partial(filename, env=None):
+def _partial(filename, env=None):
     code,res=None,None
-    #try:
-    if isinstance(filename,basestring):
-        with open('./templates/partials/'+filename+'.xml') as f:
-            raw=str(f.read())
-            #print(raw)
-            code=web.template.Template(raw)
-            if isinstance(code,web.template.Template):
-                res=str(code(env))
-            else:
-                print('not instance!')
-    #except:
-    #    print('render_partial: filename:%s env=%s'%(filename,env))
-    #finally:
-    return res
+    try:
+        if isinstance(filename,basestring):
+            with open('./templates/partials/'+filename+'.xml') as f:
+                raw=str(f.read())
+                code=web.template.Template(raw)
+                if isinstance(code,web.template.Template):
+                    res=str(code(env))
+                else:
+                    print('not instance!')
+    except:
+        print('render_partial: filename:%s env=%s'%(filename,env))
+    else:
+        if isinstance(res, basestring):
+            with open('./build/partials/'+filename+'.ui','w') as f:
+                f.write(res)
+    finally:
+        return res
+
+def partial(name, env=None):
+    render=web.template.render('./templates/partials/')
+    if 'item'==name:
+        res=str(render.item(env=env))
+        res='\n'.join([12*' '+x for x in res.split('\n')[0:-1]])[10:]
+        return res
+    return '</error func=partial>'
 
 def render():
     for template in TEMPLATES:
         with open('templates/'+template+'.xml') as f:
-            raw=f.read()
-            code=web.template.Template(raw)
+            code=web.template.render('./templates/')
+            code._add_global(partial,'render')
             with open('config/'+template+'.yml') as y:
                 data=y.read()
                 if isinstance(data, basestring):
                     env=yaml.safe_load(data)
                     with open('build/'+template+'.ui','w') as p:
-                        res=str(code(env))
+                        res=str(code.add_password_dialog(env))
                         p.write(res)
 
 def main():
     render()
-    code=render_partial('item',env={'type': 'widget',})
-    print(code)
 
 if __name__=="__main__":
     main()

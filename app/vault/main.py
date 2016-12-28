27bfdd802016b1e2329131535b0ee38427931a27
Andrew DeSantis <atd@gmx.it>
2016-12-28 13:34:18 -0600

    update-app-item:  partial templates

--- app/vault/main.py
+++ app/vault/main.py
@@ -12,18 +12,40 @@ import ruamel.yaml as yaml
 
 TEMPLATES=['add_password_dialog']
 
-def main():
+def render_partial(filename, env=None):
+    code,res=None,None
+    #try:
+    if isinstance(filename,basestring):
+        with open('./templates/partials/'+filename+'.xml') as f:
+            raw=str(f.read())
+            #print(raw)
+            code=web.template.Template(raw)
+            if isinstance(code,web.template.Template):
+                res=str(code(env))
+            else:
+                print('not instance!')
+    #except:
+    #    print('render_partial: filename:%s env=%s'%(filename,env))
+    #finally:
+    return res
+
+def render():
     for template in TEMPLATES:
         with open('templates/'+template+'.xml') as f:
             raw=f.read()
             code=web.template.Template(raw)
             with open('config/'+template+'.yml') as y:
-                rawdata=y.read()
-                if isinstance(rawdata, basestring):
-                    data=yaml.safe_load(rawdata)
+                data=y.read()
+                if isinstance(data, basestring):
+                    env=yaml.safe_load(data)
                     with open('build/'+template+'.ui','w') as p:
-                        out=str(code(data))
-                        p.write(out)
+                        res=str(code(env))
+                        p.write(res)
+
+def main():
+    render()
+    code=render_partial('item',env={'type': 'widget',})
+    print(code)
 
 if __name__=="__main__":
     main()

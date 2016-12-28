e076406a13db934bffca4e49b015b875922b93ab
Andrew DeSantis <atd@gmx.it>
2016-12-28 16:46:43 -0600

    app-vault: cleanup main.py

--- app/vault/main.py
+++ app/vault/main.py
@@ -6,45 +6,57 @@ from __future__ import division
 
 import configobj
 import jsonschema
+import os
 import web
 
 import simplejson as json
 import ruamel.yaml as yaml
 
-MACRO_BUILD="build/%s.ui"
-MACRO_CONFIG="config/%s.yml"
-MACRO_TEMPLATES="templates/%s.xml"
-PATH_TEMPLATES="./templates/"
-PATH_TEMPLATES_PARTIALS="./templates/partials/"
-TEMPLATES=["add_password_dialog"]
 
-def partial(name, env=None, factor=12):
-    render=web.template.render(PATH_TEMPLATES_PARTIALS)
+VAULT_MACRO_BUILD             = os.getenv('VAULT_MACRO_BUILD',
+                                          'build/%s.ui')
+VAULT_MACRO_CONFIG            = os.getenv('VAULT_MACRO_CONFIG',
+                                          'config/%s.yml')
+VAULT_MACRO_TEMPLATES         = os.getenv('VAULT_MACRO_TEMPLATES',
+                                          'templates/%s.xml')
+VAULT_PATH_TEMPLATES          = os.getenv('VAULT_PATH_TEMPLATES',
+                                          './templates/')
+VAULT_PATH_TEMPLATES_PARTIALS = os.getenv('VAULT_PATH_TEMPLATES_PARTIALS',
+                                          './templates/partials/')
+VAULT_TEMPLATES               = ['add_password_dialog']
+
+
+def partial(name,env=None,factor=12):
+    render=web.template.render(VAULT_PATH_TEMPLATES_PARTIALS)
     render._add_global(partial,'render')
     if 'item'==name:
         res=str(render.item(env=env,factor=factor))
         if factor==12: rm,back,div=4,0,6
         elif factor==10: rm,back,div=6,2,6
         else: rm,back,div=0,0,0
-        res='\n'.join([div*' '+x for x in res.split('\n')[0:-1]])[rm:].replace((back*' ')+'</item>','</item>')
+        res='\n'.join([div*' '+x for x in res.split(
+            '\n')[0:-1]])[rm:].replace((back*' ')+'</item>','</item>')
         return res
-    return "</error func=partial>"
+    return '</error func=partial>'
+
 
 def render():
-    for template in TEMPLATES:
-        with open(MACRO_TEMPLATES%template) as f:
-            code=web.template.render(PATH_TEMPLATES)
+    for template in VAULT_TEMPLATES:
+        with open(VAULT_MACRO_TEMPLATES%template) as f:
+            code=web.template.render(VAULT_PATH_TEMPLATES)
             code._add_global(partial,'render')
-            with open(MACRO_CONFIG%template) as y:
+            with open(VAULT_MACRO_CONFIG%template) as y:
                 data=y.read()
                 if isinstance(data, basestring):
                     env=yaml.safe_load(data)
-                    with open(MACRO_BUILD%template,'w') as p:
+                    with open(VAULT_MACRO_BUILD%template,'w') as p:
                         res=str(getattr(code,template)(env))
                         p.write(res)
+
 
 def main():
     render()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()

768bb4b5587f45b19d21931c0241187e532f2694
Andrew DeSantis <atd@gmx.it>
2016-12-28 09:12:10 -0600

    update-app: vault

--- app/vault/main.py
+++ app/vault/main.py
@@ -2,19 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
+
+import configobj
+import jsonschema
 import web
+
 import simplejson as json
 import ruamel.yaml as yaml
 
-TEMPLATES=['templates/add_password_dialog.ui']
+TEMPLATES=['add_password_dialog']
 
 def main():
     for template in TEMPLATES:
-        with open(template) as f:
+        with open('templates/'+template+'.xml') as f:
             raw=f.read()
             code=web.template.Template(raw)
-            data=None
-            print(code(data))
+            with open('config/'+template+'.yml') as y:
+                rawdata=y.read()
+                if isinstance(rawdata, basestring):
+                    data=yaml.safe_load(rawdata)
+                    with open('build/'+template+'.ui','w') as p:
+                        out=str(code(data))
+                        p.write(out)
 
 if __name__=="__main__":
     main()

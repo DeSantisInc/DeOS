da1efe1efba2e8e4468f579e599f199e1c8355da
Andrew DeSantis <atd@gmx.it>
2016-12-28 17:16:11 -0600

    update-app-vault: main.py

--- app/vault/main.py
+++ app/vault/main.py
@@ -4,13 +4,14 @@
 from __future__ import print_function
 from __future__ import division
 
+
 import configobj
 import jsonschema
 import os
-import web
-
 import simplejson as json
 import ruamel.yaml as yaml
+import web
+
 
 VAULT_MACRO_BUILD=os.getenv('VAULT_MACRO_BUILD','build/%s.ui')
 VAULT_MACRO_CONFIG=os.getenv('VAULT_MACRO_CONFIG','config/%s.yml')
@@ -19,18 +20,20 @@ VAULT_PATH_TEMPLATES=os.getenv('VAULT_PATH_TEMPLATES','./templates/')
 VAULT_PATH_PARTIALS=os.getenv('VAULT_PATH_PARTIALS','./templates/partials/')
 VAULT_TEMPLATES=['add_password_dialog']
 
-def partial(name,env=None,factor=12):
+
+def partial(name,env=None,factor=0):
+    rm,back,div=0,0,0
     render=web.template.render(VAULT_PATH_PARTIALS)
     render._add_global(partial,'render')
+    res=str(getattr(render,name)(env=env,factor=factor))
     if 'item'==name:
-        res=str(render.item(env=env,factor=factor))
-        if factor==12: rm,back,div=4,0,6
-        elif factor==10: rm,back,div=6,2,6
-        else: rm,back,div=0,0,0
-        res='\n'.join([div*' '+x for x in res.split(
-            '\n')[0:-1]])[rm:].replace((back*' ')+'</item>','</item>')
-        return res
-    return '</error func=partial>'
+        if   12==factor:rm,back,div=4,0,6
+        elif 10==factor:rm,back,div=6,2,6
+    elif 'rect'==name:  rm,back,div=6,2,6
+    res='\n'.join([div*' '+x for x in res.split('\n')[0:-1]])[rm:].replace((
+        back*' ')+'</item>','</item>')
+    return res
+
 
 def render():
     for template in VAULT_TEMPLATES:
@@ -39,14 +42,16 @@ def render():
             code._add_global(partial,'render')
             with open(VAULT_MACRO_CONFIG%template) as y:
                 data=y.read()
-                if isinstance(data, basestring):
+                if isinstance(data,basestring):
                     env=yaml.safe_load(data)
                     with open(VAULT_MACRO_BUILD%template,'w') as p:
                         res=str(getattr(code,template)(env))
                         p.write(res)
 
+
 def main():
     render()
+
 
 if __name__=="__main__":
     main()
